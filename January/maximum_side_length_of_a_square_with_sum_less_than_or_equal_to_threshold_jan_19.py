dfda8f70f963d1b5eb97c4701251f6e7149ab085
Kofi Junior Eshun <eshunkofi23@gmail.com>
2026-01-19 09:27:23 -0600

    Another day of leetcode, I will come back to this question, so help me God.

---
+++ January/maximum_side_length_of_a_square_with_sum_less_than_or_equal_to_threshold_jan_19.py
@@ -0,0 +1,40 @@
+"""
+1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold
+
+Medium
+
+Given a m x n matrix mat and an integer threshold, return the maximum side-length of a square with a sum less than or equal to threshold or return 0 if there is no such square.
+
+ 
+
+Example 1:
+
+
+Input: mat = [[1,1,3,2,4,3,2],[1,1,3,2,4,3,2],[1,1,3,2,4,3,2]], threshold = 4
+Output: 2
+Explanation: The maximum side length of square with sum less than 4 is 2 as shown.
+Example 2:
+
+Input: mat = [[2,2,2,2,2],[2,2,2,2,2],[2,2,2,2,2],[2,2,2,2,2],[2,2,2,2,2]], threshold = 1
+Output: 0
+ 
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 300
+0 <= mat[i][j] <= 104
+0 <= threshold <= 105
+"""
+from typing import List
+
+class Solution:
+    def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
+        pass
+
+def main():
+    pass 
+
+if __name__ == "__main__":
+    main()
